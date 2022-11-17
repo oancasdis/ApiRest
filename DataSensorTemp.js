29458338341bb215638f86cdaa1c9081bcc489ca
oancasdis <oancasdis@hotmail.com>
2022-11-17 20:31:44 -0300

    beginning

---
+++ DataSensorTemp.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const Tablas = require('./Tablas');
+const db = require('./db');
+
+const app = express();
+
+app.post("/DTagregar", (req, res) => {
+    db.serialize(function() {
+        db.run("INSERT INTO sensorDataTemperatura (temperaturaK, temperaturaF) VALUES (123, 123)");
+        db.run("INSERT INTO sensorDataTemperatura (temperaturaK, temperaturaF) VALUES (1234, 1243)");
+    });
+    res.end('agregar')
+});
+
+app.get("/DTobtenerUno/:id", (req, res) => {
+    // console.log(req.params.company)
+    db.serialize(function() {
+        db.each("SELECT id, temperaturaK, temperaturaF FROM sensorDataTemperatura WHERE id = ?", [req.params.id] ,function(err, row) {
+            console.log(row.id + ' ' + row.temperaturaK + ' ' + row.temperaturaF);
+        });
+    });
+    res.end('obtenerUno')
+});
+
+app.get("/DTobtenerTodos", (req, res) => {
+    db.serialize(function() {
+        db.each("SELECT id, temperaturaK, temperaturaF FROM sensorDataTemperatura", function(err, row) {
+            console.log(row.id + ' ' + row.temperaturaK + ' ' + row.temperaturaF);
+        });
+    });
+    res.end('obtenerTodos')
+});
+
+app.delete("/DTborrarUno/:id", (req, res) => {
+    // console.log(req.params.id)
+    db.serialize(function() {
+        db.run("DELETE FROM sensorDataTemperatura WHERE id = ?", [req.params.id]);
+    });
+    res.end('borrarUno')
+});
+
+app.put("/DTeditaUno/:valor/:id", (req, res) => {
+    // console.log(req.params.id)
+    db.serialize(function() {
+        db.run("UPDATE location SET temperaturaK = ? WHERE id = ?", [req.params.valor, req.params.id]);
+    });
+    res.end('EditaUno')
+});
+
+module.exports = app;
---
+++ Location.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const Tablas = require('./Tablas');
+const db = require('./db');
+
+const app = express();
+
+app.post("/Lagregar", (req, res) => {
+    db.serialize(function() {
+        db.run("INSERT INTO location (companyId, locationName, locationCountry, locationCity, locationMeta) VALUES (1, 'locationame', 'locationcountry', 'locationcity', 'locationmeta')");
+        db.run("INSERT INTO location (companyId, locationName, locationCountry, locationCity, locationMeta) VALUES (2, 'locationname1', 'locationcountry1', 'locationcity1', 'locationmeta1')");
+    });
+    res.end('agregar')
+});
+
+app.get("/LobtenerUno/:company", (req, res) => {
+    // console.log(req.params.company)
+    db.serialize(function() {
+        db.each("SELECT companyId, locationName, locationCountry, locationCity, locationMeta FROM location WHERE companyId = ?", [req.params.company] ,function(err, row) {
+            console.log(row.companyId + ' ' + row.locationName + ' ' + row.locationCountry + ' ' + row.locationCity + ' ' + row.locationMeta);
+        });
+    });
+    res.end('obtenerUno')
+});
+
+app.get("/LobtenerTodos", (req, res) => {
+    db.serialize(function() {
+        db.each("SELECT companyId, locationName, locationCountry, locationCity, locationMeta FROM location", function(err, row) {
+            console.log(row.companyId + ' ' + row.locationName + ' ' + row.locationCountry + ' ' + row.locationCity + ' ' + row.locationMeta);
+        });
+    });
+    res.end('obtenerTodos')
+});
+
+app.delete("/LborrarUno/:company", (req, res) => {
+    // console.log(req.params.company)
+    db.serialize(function() {
+        db.run("DELETE FROM location WHERE companyId = ?", [req.params.company]);
+    });
+    res.end('borrarUno')
+});
+
+app.put("/LeditaUno/:valor/:company", (req, res) => {
+    // console.log(req.params.company)
+    db.serialize(function() {
+        db.run("UPDATE location SET locationName = ? WHERE companyId = ?", [req.params.valor, req.params.company]);
+    });
+    res.end('EditaUno')
+});
+
+module.exports = app;
